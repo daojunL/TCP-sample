3c042e55c4df4d737b3d3139946cfca78110f6da
daojun <daojunliang@outlook.com>
2022-01-22 22:52:03 +0800

    [feat] add new files

---
+++ src/main/java/TCPClient.java
@@ -0,0 +1,43 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ * @author Liang_Daojun
+ * @date 2022/1/22 10:21 PM
+ */
+public class TCPClient {
+
+    public static void main(String[] args) throws IOException {
+        TCPClient tcpClient = new TCPClient();
+        SimpleDateFormat format = new SimpleDateFormat("hh-MM-ss");
+        Scanner scanner = new Scanner(System.in);
+        // 需要一直发起对server的请求 
+        while (true) {
+            String msg = scanner.nextLine();
+            if ("#".equals(msg)) break;
+            System.out.println("send time: " + format.format(new Date()));
+            System.out.println(tcpClient.sendAndReceive(TcpServer.SERVICE_IP, TcpServer.SERVICE_PORT, msg));
+            System.out.println("send time2: " + format.format(new Date()));
+        }
+    }
+
+    public String sendAndReceive(String ip, int port, String msg) throws IOException {
+        msg = msg + TcpServer.END_CHAR;
+        StringBuilder sb = new StringBuilder();
+        Socket client = new Socket(ip, port);
+        // 输出流，作为client一方，要send数据
+        OutputStream out = client.getOutputStream();
+        out.write(msg.getBytes());
+        InputStream in = client.getInputStream();
+        for (int c = in.read(); c!=TcpServer.END_CHAR; c = in.read()) {
+            if (c == -1) break;
+            sb.append((char) c);
+        }
+        return sb.toString();
+    }
+}
---
+++ src/main/java/TcpServer.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @author Liang_Daojun
+ * @date 2022/1/22 9:57 PM
+ */
+public class TcpServer {
+    public static final char END_CHAR = '#';
+    public static final String SERVICE_IP = "127.0.0.1";
+    public static final int SERVICE_PORT = 10101;
+
+    public static void main(String[] args) throws IOException {
+        TcpServer tcpServer = new TcpServer();
+        tcpServer.startService(SERVICE_IP, 10101);
+    }
+
+    public void startService(String serverIP, int serverPort) throws IOException {
+        InetAddress serverAddress = InetAddress.getByName(serverIP);
+        ServerSocket service = new ServerSocket(serverPort, 10, serverAddress);
+
+        // 需要一直响应Client传来的请求
+        while (true) {
+            StringBuilder sb = new StringBuilder();
+            // Listens for a connection to be made to this socket and accepts
+            //     * it. The method blocks until a connection is made.
+            Socket connect = service.accept();
+            // 输入流
+            InputStream in = connect.getInputStream();
+            for (int c = in.read(); c!= END_CHAR; c = in.read()) {
+                if (c == -1) break;
+                sb.append((char) c);
+            }
+
+            String response = "Hello, server has received this msg: " + sb.toString() + END_CHAR;
+            // 输出流
+            OutputStream out = connect.getOutputStream();
+            out.write(response.getBytes());
+        }
+    }
+}
